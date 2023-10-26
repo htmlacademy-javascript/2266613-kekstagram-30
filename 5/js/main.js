4d3867773296ba3105338091de607c32651e6f3e
Keks <keks@htmlacademy.ru>
2023-10-26 22:19:50 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,9 @@
+import {getPhotosDescription} from '../js/data';
+
+getPhotosDescription();
+
+// eslint-disable-next-line no-console
+console.dir(getPhotosDescription, { depth: null });
+
+// eslint-disable-next-line no-console
+console.log(getPhotosDescription());
