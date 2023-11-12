33c886b635e77102a8e4a3788f6e6237c57b1d37
Keks <keks@htmlacademy.ru>
2023-11-12 15:32:18 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,7 @@
+import { getPicturesData } from '../js/data.js'; // Функция для получения случайных данных
+import { renderGallery } from './renderGallery.js'; // Функция для создания галереи
+import './uploadPictureForm.js'; // Модуль для работы формы загрузки изображения
+
+const arrayDataPictures = getPicturesData();
+
+renderGallery(arrayDataPictures);
